fcff184b93b2865805187225240a82307137bcf7
Gabriel Bonner <gabrielbonner@gmail.com>
2016-10-23 16:40:51 -0700

    Tidy up and pseudocode a solution

--- magic_morning_22_oct.rb
+++ magic_morning_22_oct.rb
@@ -1,11 +1,14 @@
 def sort_rubies(input, players)
-  # shares = Array.new(players, [])
+
+  # create 'players' in a nested array
   shares = []
   target = input.reduce(:+)/players
   players.times do
     shares << [0]
   end
 
+  # roughly sort the rubies to get an approximate even value bewteen players
+  # only works with 2 players at the moment
   until input.length == 0
     if shares[0].reduce(:+) < shares[1].reduce(:+)
       shares[0] << input.pop
@@ -17,7 +20,8 @@ def sort_rubies(input, players)
   end
 
 
-  # if any number differential between two numbers == targetdiff, switch em
+  #
+  # only works for two players
   if shares[0].reduce(:+) > shares[1].reduce(:+)
     higher_value_array = shares[0]
     lower_value_array = shares[1]
@@ -26,8 +30,14 @@ def sort_rubies(input, players)
     lower_value_array = shares[0]
   end
 
+  # find the target number that will equalize the shares
+  # only works for two people at the moment
   target_differential = ((shares[0].reduce(:+) - shares[1].reduce(:+)) / 2).abs
 
+
+
+  # check for a one-swap-winner-scenario
+  # only works for two players
   higher_value_array.each do |num1|
     lower_value_array.each do |num2|
       if num1 - num2 == target_differential
@@ -42,13 +52,20 @@ def sort_rubies(input, players)
     end
   end
 
+# to solve:
+# generate all possible permutations of higher_value_array, push to new array1
+# generate all possible permutations of lower_value_array, push to new array2
+# if any of the possible permutations of array1 - array2 = target differential, print "There is a possible solution"
+# now the hard part - figure out how to identify and switch the numbers that will give you the solution
+# hint: use a hash table to store the total of each permutation as the key, and the value would be an array of the numbes that give that total.  Then you can go about switching the numbers in the the two value which correspond to the keys that give the target differential...easy peazy!
 
-  sum_totals(higher_value_array, lower_value_array)
-  check_for_easy_win(shares)
+
+  pretty_print(higher_value_array, lower_value_array)
+  check_for_win(shares)
 
 end
 
-def sum_totals(higher_value_array, lower_value_array)
+def pretty_print(higher_value_array, lower_value_array)
   puts "1: #{higher_value_array.reduce(:+)}"
   puts "1: #{higher_value_array}"
   puts
@@ -56,7 +73,7 @@ def sum_totals(higher_value_array, lower_value_array)
   puts "2: #{lower_value_array}"
 end
 
-def check_for_easy_win(shares)
+def check_for_win(shares)
   # MVP, expand to take more than 2 players
   if shares[0].reduce(:+) == shares[1].reduce(:+)
     puts "Success!"
@@ -65,6 +82,7 @@ def check_for_easy_win(shares)
   end
 end
 
+####################### runner code ########################
 input = ARGV
 input.map! {|i| i.to_i}
 players = input.shift
@@ -78,3 +96,10 @@ else
     sort_rubies(input, players)
   end
 end
+
+
+#####################
+##### test cases ########
+#####################
+
+# ruby magic_morning_22_oct.rb 2
